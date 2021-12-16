c2f76d6090cc359d6e710ebe319b034ea3013f1b
DINH CHI CONG <dinhchicongf9@gmail.com>
2021-12-16 13:03:09 +0700

    create

---
+++ lib_all.py
@@ -0,0 +1,20 @@
+import os
+import random
+import xml.etree.ElementTree as ET
+import cv2
+import torch
+import torch.utils.data as data
+import os
+import numpy as np
+#gửi request tới trang chứa đường link download của mình
+import urllib.request
+# File download về là file zip nên phải unzip ra
+import zipfile
+# Nếu là file tar thì dùng cái dưới
+import tarfile
+
+# Cài đặt các tham số ngẫu nhiên
+torch.manual_seed(1234)
+np.random.seed(1234)
+random.seed(1234)
+
