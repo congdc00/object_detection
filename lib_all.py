b86e8eda06e5f85a0d7250001c956119f3ae7fbf
DINH CHI CONG <dinhchicongf9@gmail.com>
2021-12-30 22:52:43 +0700

    update
    video 16

--- lib_all.py
+++ lib_all.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 import cv2
 import torch
 import torch.utils.data as data
+from torch.autograd import Function # tu dong tinh gradient
 import os
 import numpy as np
 #noron network
@@ -28,4 +29,24 @@ import itertools
 torch.manual_seed(1234)
 np.random.seed(1234)
 random.seed(1234)
+
+import os
+import os.path as osp
+
+import random
+import xml.etree.ElementTree as ET
+import cv2
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as data
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+import itertools
+from math import sqrt
+import time
 
